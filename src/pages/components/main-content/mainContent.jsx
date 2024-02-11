ef7aa623e1fd8b17206b31bbb8415af8179a6116
Yogurt7v <egorov-2k@yandex.ru>
2024-02-11 20:22:03 +0300

    замена окна притяжения

--- src/pages/components/main-content/mainContent.jsx
+++ src/pages/components/main-content/mainContent.jsx
@@ -3,14 +3,15 @@ import style from "./mainContent.module.css";
 import Slider from "../slider/Slider";
 import Card from "../card/Card";
 import SortBar from "../sort-bar/sort";
-import Phone from "../main-content/Image/phone.svg";
+import Discount from "../main-content/Image/discount.svg";
+
 
 export const MainContent = () => {
   return (
     <>
       <div className={style.MainContentWrapper}>
         <div className={style.SliderWrapper}>
-          <button className={style.PhoneButton}><img src={Phone} alt="Phone" className={style.Phone}/>+7 (999) 888-77-66</button>
+          <button className={style.PhoneButton}><img src={Discount} alt="discount" className={style.Phone}/>Discount for registration</button>
           <Slider />
         </div>
         <div className={style.Content}>
