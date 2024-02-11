3956fcdefad5a223a5cb68cb0fee7984048d6920
Yogurt7v <egorov-2k@yandex.ru>
2024-02-11 18:52:42 +0300

    add phone button

--- src/pages/components/main-content/mainContent.jsx
+++ src/pages/components/main-content/mainContent.jsx
@@ -3,12 +3,14 @@ import style from "./mainContent.module.css";
 import Slider from "../slider/Slider";
 import Card from "../card/Card";
 import SortBar from "../sort-bar/sort";
+import Phone from "../main-content/Image/phone.svg";
 
 export const MainContent = () => {
   return (
     <>
       <div className={style.MainContentWrapper}>
         <div className={style.SliderWrapper}>
+          <button className={style.PhoneButton}><img src={Phone} alt="Phone" className={style.Phone}/>+7 (999) 888-77-66</button>
           <Slider />
         </div>
         <div className={style.Content}>
