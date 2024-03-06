b9e8a753ba4f2bb14a1770c5516075f5cb4da5b2
Yogurt7v <egorov-2k@yandex.ru>
2024-03-06 20:14:41 +0300

    видео поправлено

--- src/pages/components/video-background.js/video-background.js
+++ src/pages/components/video-background.js/video-background.js
@@ -4,14 +4,10 @@ export const VideoBackground = () => {
   return (
     <>
       <iframe
-        сlassName={style.videoBackground}
-        src="https://www.youtube.com/embed/gKKLbQbXpw4?si=_2nePNO1nIQNEpzV&amp;controls=0"
+        className={style.videoBackground}
+        src="https://www.youtube.com/embed/gKKLbQbXpw4?autoplay=1&mute=1&controls=0&loop=1&playlist=gKKLbQbXpw4"
         title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; muted; loop; picture-in-picture; web-share"
-        allowfullscreen
       ></iframe>
     </>
   );
 };
-
