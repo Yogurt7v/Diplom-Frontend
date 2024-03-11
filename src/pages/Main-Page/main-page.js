d355d78aec415eeee5f54e8d1cf8d53cb4fb7a5c
Yogurt7v <egorov-2k@yandex.ru>
2024-03-11 16:16:31 +0300

    some fix

--- src/pages/Main-Page/main-page.js
+++ src/pages/Main-Page/main-page.js
@@ -5,28 +5,28 @@ import SearchBar from "../components/search-bar/search-bar.js";
 import { useLayoutEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setUser } from "../../actions";
-import { VideoBackground } from "../components"
+import { VideoBackground } from "../components";
 import { useState } from "react";
 import { useEffect } from "react";
 import { useServerRequest } from "../../hooks/use-server-request";
 import { PAGINATION_LIMIT } from "../../constants/pagination-limit";
 import { getLastPageFromLinks } from "../../utils/getLastPageFromLinks";
-
-
+import { SortBar } from "../components/sort-bar";
 
 export const MainPage = () => {
   const dispatch = useDispatch();
 
-  const [ searchPhrase, setSearchPhrase ] = useState("");
-  const [searchPhraseFromSearchBar, setSearchPhraseFromSearchBar] = useState("");
+  const [searchPhrase, setSearchPhrase] = useState("");
+  const [searchPhraseFromSearchBar, setSearchPhraseFromSearchBar] =
+    useState("");
 
   const onSearch = () => {
-    setSearchPhrase(searchPhraseFromSearchBar)
+    setSearchPhrase(searchPhraseFromSearchBar);
   };
 
-   const onDelete = () => {
-    setSearchPhrase("")
-   }
+  const onDelete = () => {
+    setSearchPhrase("");
+  };
 
   useLayoutEffect(() => {
     const currentUserDataJSON = sessionStorage.getItem("userData");
@@ -38,12 +38,11 @@ export const MainPage = () => {
     dispatch(
       setUser({ ...currentUserData, roleId: Number(currentUserData.roleId) })
     );
-  }, [dispatch]);  
+  }, [dispatch]);
   const [products, setProducts] = useState([]);
   const requestServer = useServerRequest();
   const [page, setPage] = useState(1);
   const [lastPage, setLastPage] = useState(1);
-
 
   useEffect(() => {
     requestServer(`fetchProducts`, searchPhrase, page, PAGINATION_LIMIT).then(
@@ -57,12 +56,24 @@ export const MainPage = () => {
   return (
     <>
       <div className={style.AppWrapper}>
-        <SearchBar searchPhraseFromSearchBar={searchPhraseFromSearchBar} setSearchPhraseFromSearchBar={setSearchPhraseFromSearchBar} searchPhrase={searchPhrase} onSearch={onSearch} onDelete ={onDelete}/>
-        <Content products={products} page={page} setPage={setPage} lastPage={lastPage}/>
-        <VideoBackground/>
+        <div className={style.SortBarWrapper}>
+          <SortBar />
+          <SearchBar
+            searchPhraseFromSearchBar={searchPhraseFromSearchBar}
+            setSearchPhraseFromSearchBar={setSearchPhraseFromSearchBar}
+            searchPhrase={searchPhrase}
+            onSearch={onSearch}
+            onDelete={onDelete}
+          />
+        </div>
+        <Content
+          products={products}
+          page={page}
+          setPage={setPage}
+          lastPage={lastPage}
+        />
+        <VideoBackground />
       </div>
     </>
   );
-}
-
-
+};
--- src/pages/components/sort-bar/sort.js
+++ src/pages/components/sort-bar/sort.js
@@ -1,10 +1,21 @@
 import style from "./sort.module.css";
+import price from "../../../icons/price.svg"
+import weight from "../../../icons/weight.svg"
+import Kcal from "../../../icons/calories.svg"
+
+
 export const SortBar = () => {
   return (
     <div className={style.SortBarContent}>
-      <span className={style.SortBarSpan}>by price</span>
-      <span className={style.SortBarSpan}>by quantity</span>
-      <span className={style.SortBarSpan}>by rating</span>
+      <div className={style.SortBarSpan}>
+        <img src={price} alt="price" className={style.SortBarIcon}></img>
+      </div>
+      <div className={style.SortBarSpan}>
+        <img src={weight} alt="weight" className={style.SortBarIcon}></img>
+      </div>
+      <div className={style.SortBarSpan}>
+        <img src={Kcal} alt="Kcal" className={style.SortBarIcon}></img>
+      </div>
     </div>
   );
 };
