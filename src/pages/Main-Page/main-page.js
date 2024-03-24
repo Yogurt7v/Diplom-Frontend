5843342df6f5c7ceaf3b4f4d2c16290c94fe4a01
Yogurt7v <egorov-2k@yandex.ru>
2024-03-24 12:13:31 +0300

    пробуем делать пагинацию

--- src/pages/Main-Page/main-page.js
+++ src/pages/Main-Page/main-page.js
@@ -18,37 +18,6 @@ export const MainPage = () => {
   const [sorting, setSorting] = useState("");
   const [searchCategory, setSearchCategory] = useState("");
 
-  //   {
-  //     value: "priceDESC",
-  //     label: "по цене по убыванию",
-  //     sort: (data) => _.orderBy(data, ["price"], ["desc"]),
-  //   },
-  //   {
-  //     value: "priceASC",
-  //     label: "по цене по возрастанию",
-  //     sort: (data) => _.orderBy(data, ["price"], ["asc"]),
-  //   },
-  //   {
-  //     value: "weightASC",
-  //     label: "по весу по возрастанию",
-  //     sort: (data) => _.orderBy(data, ["weight"], ["asc"]),
-  //   },
-  //   {
-  //     value: "weightDESC",
-  //     label: "по весу по убыванию",
-  //     sort: (data) => _.orderBy(data, ["weight"], ["desc"]),
-  //   },
-  //   {
-  //     value: "caloriesASC",
-  //     label: "по калорийности по возрастанию",
-  //     sort: (data) => _.orderBy(data, ["calories"], ["asc"]),
-  //   },
-  //   {
-  //     value: "caloriesDESC",
-  //     label: "по калорийности по убыванию",
-  //     sort: (data) => _.orderBy(data, ["calories"], ["desc"]),
-  //   },
-  // ];
 
   const sortOption = SORT_OPTIONS;
 
