8866615040b650e0f7234768e6b553563f2ab181
Yogurt7v <egorov-2k@yandex.ru>
2024-05-05 11:57:53 +0300

    some adaptive

--- src/pages/components/sort-bar/sort.js
+++ src/pages/components/sort-bar/sort.js
@@ -2,10 +2,10 @@ import style from "./sort.module.css";
 
 export const SortBar = ({ onSort, options }) => {
   return (
-    <div className={style.SortBarWrapper}>
+    <div className={style.SortBarWrap}>
       <span className={style.SortBarTitle}>Сортировка</span>
       <select onChange={onSort} className={style.SortBarSelect}>
-        <option defaultValue="" disabled selected hidden className={style.SortBarOption}>
+        <option value="" className={style.SortBarOption}>
         </option>
         {options.map((option) => (
           <option value={option.value} key={option.value} className={style.SortBarOption}>
