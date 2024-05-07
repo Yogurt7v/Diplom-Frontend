bab91fb3884958a0d4efc87b8befac0e9e5a9b91
Yogurt7v <egorov-2k@yandex.ru>
2024-05-07 14:46:48 +0300

    done

--- src/pages/Busket-Page/busket.js
+++ src/pages/Busket-Page/busket.js
@@ -85,7 +85,6 @@ export const Busket = () => {
         },
         onCancel: () => {
           dispatch(CLOSE_MODAL);
-          dispatch(clearBusketData());
           navigate("/");
         },
       })
